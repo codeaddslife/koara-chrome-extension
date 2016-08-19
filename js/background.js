7979c7a5d626204440a4b86eb54756cdbabbfac5
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-08-19 11:00:30 +0200

    tryout

--- js/background.js
+++ js/background.js
@@ -1,1 +1,8 @@
-console.log('BACKGROUND');
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // No tabs or host permissions needed!
+    console.log('Turning ' + tab.url + ' red!');
+    chrome.tabs.executeScript({
+        code: 'document.body.style.backgroundColor="green"'
+    });
+});
