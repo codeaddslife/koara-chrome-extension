617e20c87f6e62e44ae295626d22838d4c2a3a3d
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-08-29 17:31:03 +0200

    Fix content issue

--- extension/js/content.js
+++ extension/js/content.js
@@ -5,12 +5,12 @@
     document.body.removeChild(pre);
 
     var parser = new koara.Parser();
-    var doc = parser.parse(pre.innerHTML.trim());
+    var doc = parser.parse(pre.innerHTML);
     var renderer = new koaraHtml.Html5Renderer();
     doc.accept(renderer);
 
     var content = document.createElement('div') ;
     content.innerHTML = renderer.getOutput();
     document.body.appendChild(content);
-    
+
 })();
